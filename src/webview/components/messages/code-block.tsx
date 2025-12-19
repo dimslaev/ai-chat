e47c65662809266f28e5224196876e53c2d0fa0a
Dimitar Slaev <dimslaev@gmail.com>
2025-12-19 13:36:05 +0100

    remove code block max height

--- src/webview/components/messages/code-block.tsx
+++ src/webview/components/messages/code-block.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import { CopyButton } from "@/components/ui/copy-button";
-import { Badge, IconButton, Box, Flex } from "@radix-ui/themes";
-import { ChevronDownIcon, ChevronUpIcon } from "@radix-ui/react-icons";
-
-const MAX_HEIGHT = 300;
+import { Badge, Box, Flex } from "@radix-ui/themes";
 
 export const CodeBlock: React.FC<{
   children: React.ReactNode;
@@ -11,8 +8,6 @@ export const CodeBlock: React.FC<{
 }> = ({ children, className }) => {
   const codeRef = React.useRef<HTMLElement>(null);
   const contentRef = React.useRef<HTMLDivElement>(null);
-  const [isExpanded, setIsExpanded] = React.useState(false);
-  const [hasOverflow, setHasOverflow] = React.useState(true);
 
   const language = React.useMemo(() => {
     if (!className) return null;
@@ -21,15 +16,6 @@ export const CodeBlock: React.FC<{
     const lang = match[1];
     return lang.charAt(0).toUpperCase() + lang.slice(1);
   }, [className]);
-
-  const hasSetOverflow = React.useRef(false);
-  React.useLayoutEffect(() => {
-    if (contentRef.current && hasSetOverflow.current === false) {
-      const contentHeight = contentRef.current.scrollHeight;
-      hasSetOverflow.current = true;
-      setHasOverflow(contentHeight > MAX_HEIGHT);
-    }
-  }, [children]);
 
   return (
     <Box className="code-block-container">
@@ -49,32 +35,11 @@ export const CodeBlock: React.FC<{
         <CopyButton elRef={codeRef} />
       </Flex>
 
-      <Box
-        ref={contentRef}
-        className={`code-block-content ${
-          !isExpanded && hasOverflow ? "code-block-collapsed" : ""
-        }`}
-        style={{
-          maxHeight: !isExpanded && hasOverflow ? `${MAX_HEIGHT}px` : "none",
-        }}
-      >
+      <Box ref={contentRef} className="code-block-content">
         <code ref={codeRef} className={className}>
           {children}
         </code>
       </Box>
-
-      {hasOverflow && (
-        <Flex align="center" justify="center" className="code-block-footer">
-          <IconButton
-            size="1"
-            variant="ghost"
-            color="gray"
-            onClick={() => setIsExpanded(!isExpanded)}
-          >
-            {!isExpanded ? <ChevronDownIcon /> : <ChevronUpIcon />}
-          </IconButton>
-        </Flex>
-      )}
     </Box>
   );
 };
